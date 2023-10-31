3a40643bc8bb81911bafb5a70adeef0fbfe3523d
MaxPredathor <max.gilli98@gmail.com>
2023-10-31 14:20:02 +0100

    added remove and add task functions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,40 @@
 const { createApp } = Vue;
 createApp({
   data() {
-    return {};
+    return {
+      tasks: [
+        {
+          text: "a",
+          done: false,
+          id: 1,
+        },
+        {
+          text: "b",
+          done: false,
+          id: 2,
+        },
+        {
+          text: "c",
+          done: false,
+          id: 3,
+        },
+      ],
+      nextId: 4,
+      todoText: "",
+    };
   },
-  methods: {},
+  methods: {
+    removeTask(id) {
+      const index = this.tasks.findIndex((task) => task.id === id);
+      this.tasks.splice(index, 1);
+    },
+    addTask() {
+      this.tasks.push({
+        text: this.todoText,
+        done: false,
+        id: this.nextId++,
+      });
+      this.todoText = "";
+    },
+  },
 }).mount("#app");
